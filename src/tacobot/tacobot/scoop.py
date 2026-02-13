6435f864ecc25783b2cd19cb83c2e6f0207e7343
hooneyskywalker0127 <hooney0127@gmail.com>
2026-02-13 16:18:14 +0900

    Create scoop.py
    movej

---
+++ src/tacobot/tacobot/scoop.py
@@ -0,0 +1,105 @@
+import rclpy
+import DR_init
+import time
+
+# 로봇 설정 상수 (필요에 따라 수정)
+ROBOT_ID = "dsr01"
+ROBOT_MODEL = "m0609"
+ROBOT_TOOL = "Tool Weight"
+ROBOT_TCP = "GripperDA_v1"
+
+# 이동 속도 및 가속도 (필요에 따라 수정)
+VELOCITY = 40
+ACC = 60
+
+# DR_init 설정
+DR_init.__dsr__id = ROBOT_ID
+DR_init.__dsr__model = ROBOT_MODEL
+
+
+def initialize_robot():
+    """로봇의 Tool과 TCP를 설정"""
+    from DSR_ROBOT2 import set_tool, set_tcp,get_tool,get_tcp,ROBOT_MODE_MANUAL,ROBOT_MODE_AUTONOMOUS  # 필요한 기능만 임포트
+    from DSR_ROBOT2 import get_robot_mode,set_robot_mode
+
+    # Tool과 TCP 설정시 매뉴얼 모드로 변경해서 진행
+    set_robot_mode(ROBOT_MODE_MANUAL)
+    set_tool(ROBOT_TOOL)
+    set_tcp(ROBOT_TCP)
+    
+    set_robot_mode(ROBOT_MODE_AUTONOMOUS)
+    time.sleep(2)  # 설정 안정화를 위해 잠시 대기
+    # 설정된 상수 출력
+    print("#" * 50)
+    print("Initializing robot with the following settings:")
+    print(f"ROBOT_ID: {ROBOT_ID}")
+    print(f"ROBOT_MODEL: {ROBOT_MODEL}")
+    print(f"ROBOT_TCP: {get_tcp()}") 
+    print(f"ROBOT_TOOL: {get_tool()}")
+    print(f"ROBOT_MODE 0:수동, 1:자동 : {get_robot_mode()}")
+    print(f"VELOCITY: {VELOCITY}")
+    print(f"ACC: {ACC}")
+    print("#" * 50)
+
+
+
+def scoop():
+    """로봇이 수행할 작업"""
+    print("Performing task...")
+    from DSR_ROBOT2 import posx,posj,movec,movej,move_periodic,movel,set_ref_coord,wait,DR_TOOL  # 필요한 기능만 임포트
+
+    # 초기 위치 및 목표 위치 설정
+
+    P0 = posj(0,0,90,0,90,90)
+    P1 = posx(367.34, 4.86, 125.75, 111.67, 179.79, -157.88)
+    P2 = posx(589.75, 16.62, 90.35, 152.95, 174.37, -116.14)
+
+#pos1 = posx([500, 80, 200, 150, 179, 150])
+#JReady = [0, 0, 90, 0, 90, 0]
+
+    # 반복 동작 수행
+    movej(P0,v=80, a=70)
+
+    for i in range(5):
+        movec(P1, P2, vel=120, acc=100)
+        movej(P0, vel=100, acc=90)
+
+    movej(P0,v=80, a=70)
+
+    for i in range(3):
+        movec(P1, P2, v=120, a=100)
+        movej(P0,v=100, a=90)
+
+    # move_periodic(
+    #     amp=[30, 0, 0, 0, 0, 0],
+    #     period=2.0,
+    #     atime=1,
+    #     repeat=3,
+    #     ref=DR_TOOL
+    # )
+    
+
+def main(args=None):
+    """메인 함수: ROS2 노드 초기화 및 동작 수행"""
+    rclpy.init(args=args)
+    node = rclpy.create_node("move_basic", namespace=ROBOT_ID)
+
+    # DR_init에 노드 설정
+    DR_init.__dsr__node = node
+
+    try:
+        # 초기화는 한 번만 수행
+        initialize_robot()
+
+        # 작업 수행 (한 번만 호출)
+        scoop()
+
+    except KeyboardInterrupt:
+        print("\nNode interrupted by user. Shutting down...")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
